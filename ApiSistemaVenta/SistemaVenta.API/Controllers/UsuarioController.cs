4f7bc91d97d61260f3d078b4845164992706b1de
JoseChatataJallo2004 <jchatatajallo@gmail.com>
2024-08-25 12:08:46 -0500

    create

---
+++ ApiSistemaVenta/SistemaVenta.API/Controllers/UsuarioController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.API.Utilidad;
+using SistemaVenta.BLL.Servicios;
+
+namespace SistemaVenta.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuario)
+        {
+            _usuarioService = usuario;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            var rsp = new Response<List<UsuarioDTO>>();
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _usuarioService.Lista();
+            }
+            catch (Exception ex)
+            {
+                rsp.status = false;
+                rsp.msg = ex.Message;
+
+            }
+            return Ok(rsp);
+        }
+
+
+        [HttpPost]
+        [Route("IniciarSesion")]
+        public async Task<IActionResult> IniciarSesion([FromBody] LoginDTO loginDTO)
+        {
+            var rsp = new Response<SesionDTO>();
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _usuarioService.ValidarCredenciales(loginDTO.Correo,loginDTO.Clave);
+            }
+            catch (Exception ex)
+            {
+                rsp.status = false;
+                rsp.msg = ex.Message;
+
+            }
+            return Ok(rsp);
+        }
+
+
+        [HttpPost]
+        [Route("Guardar")]
+        public async Task<IActionResult> Guardar([FromBody] UsuarioDTO usuarioDTO)
+        {
+            var rsp = new Response<UsuarioDTO>();
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _usuarioService.Crear(usuarioDTO);
+            }
+            catch (Exception ex)
+            {
+                rsp.status = false;
+                rsp.msg = ex.Message;
+
+            }
+            return Ok(rsp);
+        }
+
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public async Task<IActionResult> Actualizar([FromBody] UsuarioDTO usuarioDTO)
+        {
+            var rsp = new Response<bool>();
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _usuarioService.Editar(usuarioDTO);
+            }
+            catch (Exception ex)
+            {
+                rsp.status = false;
+                rsp.msg = ex.Message;
+
+            }
+            return Ok(rsp);
+        }
+
+
+        [HttpDelete]
+        [Route("Eliminar/{id:int}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var rsp = new Response<bool>();
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _usuarioService.Eliminar(id);
+            }
+            catch (Exception ex)
+            {
+                rsp.status = false;
+                rsp.msg = ex.Message;
+
+            }
+            return Ok(rsp);
+        }
+    }
+}
---
+++ ApiSistemaVenta/SistemaVenta.BLL/Servicios/ProductoService.cs
@@ -0,0 +1,119 @@
+﻿using SistemaVenta.BLL.Servicios.Contrato;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaVenta.BLL.Servicios
+{
+    public class ProductoService :IProductoService
+    {
+        private readonly IGenericRepository<Producto> _productoRepositorio;
+        private readonly IMapper _mapper;
+
+        public ProductoService(IGenericRepository<Producto> productoRepositorio, IMapper mapper)
+        {
+            _productoRepositorio = productoRepositorio;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductoDTO> Crear(ProductoDTO modelo)
+        {
+            try
+            {
+                var productoCreado = await _productoRepositorio.Crear(_mapper.Map<Producto>(modelo));
+                if (productoCreado.IdProducto == 0)
+                    throw new TaskCanceledException("El producto no creado");
+
+               
+                return _mapper.Map<ProductoDTO>(productoCreado);
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<bool> Editar(ProductoDTO modelo)
+        {
+            try
+            {
+                var productoModelo = _mapper.Map<Producto>(modelo);
+
+                var productoEncontrado = await _productoRepositorio.Obtener(u => u.IdProducto == productoModelo.IdProducto);
+
+                if (productoEncontrado == null)
+                    throw new TaskCanceledException("El producto no existe");
+
+                productoEncontrado.Nombre = productoModelo.Nombre;
+                productoEncontrado.IdCategoria = productoModelo.IdCategoria;
+                productoEncontrado.Stock = productoModelo.Stock;
+                productoEncontrado.Precio = productoModelo.Precio;
+                productoEncontrado.EsActivo = productoModelo.EsActivo;
+
+                bool respuesta = await _productoRepositorio.Editar(productoEncontrado);
+
+
+                if (!respuesta)
+                    throw new TaskCanceledException("No se puedo editar");
+
+                return respuesta;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            try
+            {
+                var productoEncontrado = await _productoRepositorio.Obtener(u => u.IdProducto == id);
+
+                if (productoEncontrado == null)
+                    throw new TaskCanceledException("El producto no existe");
+                bool respuesta = await _productoRepositorio.Eliminar(productoEncontrado);
+
+
+                if (!respuesta)
+                    throw new TaskCanceledException("No se puedo eliminar");
+
+                return respuesta;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<List<ProductoDTO>> Lista()
+        {
+            try
+            {
+                var queryproducto = await _productoRepositorio.Consultar();
+                var listaproductos = queryproducto.Include(cat => cat.IdCategoriaNavigation).ToList();
+
+                return _mapper.Map<List<ProductoDTO>>(listaproductos.ToList());
+            }
+            catch
+            {
+
+                throw;
+            }
+        }
+    }
+}
